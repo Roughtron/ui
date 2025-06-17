cd29cd9d03f3081ca325e7ca57786011cff549de
Roughtron <roughtron@yandex.ru>
2025-06-17 09:25:43 -0300

    add index file

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as XLoader } from './XLoader.vue';
+export { default as XSvgIcon } from './XSvgIcon.vue';
+export { default as XButton } from './XButton.vue';
+export * from './types';
